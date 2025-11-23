3245bee3756ab77a6d6d6bcd417b3c0bf5523dac
shivamagrawal06 <76582390+shivamagrawal06@users.noreply.github.com>
2025-11-24 00:30:00 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+first_number=int(input("enter first number:"))
+second_number=int(input("enter second number:"))
+#additon
+a=first_number+second_number
+print("Addition:",a)
+#subtraction
+b=first_number-second_number
+print("Subtraction:",b)
+#multiplication
+c=first_number*second_number
+print("Multiplication:",c)
+#division
+d=first_number/second_number
+print("Division:",d)
---
+++ task2.py
@@ -0,0 +1,3 @@
+first=input("enter first name:")
+last=input("enter last name:")
+print("Hello,",first+"!","welcome to the python program")
